127ae738c32f0578ee5afbd1a87b624b12b53ce1
dev-shahalam <mdshahalam8283@gmail.com>
2024-11-01 02:20:00 +0600

    V1.0.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-class UserController extends Controller
-{
-    //
+class UserController extends Controller {
+    public function homePage(){
+        return view("home_page");
+    }
 }
---
+++ resources/views/home_page.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>3s Impex Bangladesh</title>
+</head>
+<body>
+
+    <div>
+        <h5>3S Impex Bangladesh | Foreign Managements</h5>
+    </div>
+
+    <div>
+
+        <h1>1st Company</h1>
+
+
+    </div>
+
+
+</body>
+</html>
